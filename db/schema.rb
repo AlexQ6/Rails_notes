6758484e35e4847cc570184f5b482d51d1fa85d5
Alex Qiang <alex.qiang36@gmail.com>
2020-07-03 00:49:48 +1000

    Generate user model and users controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_23_051210) do
+ActiveRecord::Schema.define(version: 2020_07_02_144434) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,14 @@ ActiveRecord::Schema.define(version: 2020_06_23_051210) do
   create_table "notes", force: :cascade do |t|
     t.string "title"
     t.string "content"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
